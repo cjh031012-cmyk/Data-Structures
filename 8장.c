9fa788eefe2c44208275fb21c85c9329d7e91629
최종화 <cjh031012@gmail.com>
2026-05-18 14:37:06 +0900

    Update 8장.c

--- 8장.c
+++ 8장.c
@@ -24,3 +24,32 @@ int main(void)
 
 	return 0;
 }
+
+///15번
+int find_smaller(TreeNode* root, int value) {
+	if (root == NULL)
+		return 0;
+	if (root->key < value)
+		printf("%d보다 작은 노드: %d\n", value, root->key);
+	find_smaller(root->left, value);
+	find_smaller(root->right, value);
+}
+int main(void)
+{
+	TreeNode* root = NULL;
+	TreeNode* tmp = NULL;
+
+	root = insert_node(root, 30);
+	root = insert_node(root, 20);
+	root = insert_node(root, 10);
+	root = insert_node(root, 40);
+	root = insert_node(root, 50);
+	root = insert_node(root, 60);
+
+	int value = 0;
+	printf("값을 입력하시오:");
+	scanf("%d", &value);
+	int smaller = find_smaller(root, value);
+
+	return 0;
+}
